cd4bf6851068e009d20ad715c90f8982dca8aa67
Tyler Holewinski <tyler.holewinski@bryx.com>
2024-03-04 21:46:19 -0500

    fix(notary): set content type for json responses

--- lib/notary/helpers.ex
+++ lib/notary/helpers.ex
@@ -4,6 +4,9 @@ defmodule Notary.Helpers do
   @spec send_json(Plug.Conn.t(), map(), Keyword.t()) :: Plug.Conn.t()
   def send_json(conn, body, options \\ []) do
     status = options |> Keyword.get(:code, 200)
-    conn |> send_resp(status, body |> Jason.encode!())
+
+    conn
+    |> put_resp_header("content-type", "application/json")
+    |> send_resp(status, body |> Jason.encode!())
   end
 end
