e24da3b1286dd2b98e622f5c88ed18a6623787f5
Tyler Holewinski <tyler@holewinski.dev>
2025-03-08 13:03:25 -0500

    feat(notary): export zod schema from @erwijet/notary

---
+++ client/.changeset/light-carrots-greet.md
@@ -0,0 +1,5 @@
+---
+"@erwijet/notary": patch
+---
+
+Publishes `userInfoSchema`
--- client/src/index.ts
+++ client/src/index.ts
@@ -2,3 +2,4 @@ import { z } from "zod";
 import { userInfoSchema } from "./schemas";
 export { createNotary, type Notary } from "./notary";
 export type UserInfo = z.infer<typeof userInfoSchema>;
+export { userInfoSchema }
